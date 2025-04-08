04677c29b079ba9ed3c4b49c236a1465dc44475b
Breno <brnodionisio23@gmail.com>
2025-04-08 11:14:21 -0300

    Criação da classe: calendario

---
+++ backend/src/main/java/com/brenodev/pilates/model/Calendar.java
@@ -0,0 +1,30 @@
+package com.brenodev.pilates.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity(name = "tb_calendario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Calendar implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "data")
+    private LocalDate date;
+
+    @Column(name = "hora")
+    private LocalTime time;
+
+}
