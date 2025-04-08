211f6759cf44514bffb207d49d3b45338b692484
Breno <brnodionisio23@gmail.com>
2025-04-08 11:03:54 -0300

    Criação da classe: professor

---
+++ backend/src/main/java/com/brenodev/pilates/model/Teacher.java
@@ -0,0 +1,10 @@
+package com.brenodev.pilates.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_professor")
+public class Teacher extends Person {
+
+}
