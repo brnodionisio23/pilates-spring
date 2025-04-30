82b7dd308e356099fd2629b9ece3806aa64a108b
Breno <brnodionisio23@gmail.com>
2025-04-30 20:29:04 -0300

    atualizando tabela professor

--- backend/src/main/java/com/brenodev/pilates/model/Teacher.java
+++ backend/src/main/java/com/brenodev/pilates/model/Teacher.java
@@ -3,6 +3,7 @@ package com.brenodev.pilates.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.CascadeType;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,7 +12,7 @@ import java.util.Set;
 @Table(name = "tb_professor")
 public class Teacher extends Person {
 
-    @OneToMany(mappedBy = "teacher")
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Schedule> scheduleSet = new HashSet<>();
 
 }
